5e04548e2a56e16cec2e6f5a674572bd253bed41
Ibrahim <ibrahimrathwala@gmail.com>
2024-05-02 11:09:10 +0400

    Updates

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -82,7 +82,7 @@ const projects = () => {
                 </title>
                 <meta name='description' content='Few of my projects...' />
             </Head>
-            <main className='w-full mb-16 flex flex-col items-center justify-center'>
+            <main className='w-full flex flex-col items-center justify-center'>
                 <Layout className='pt-16'>
                     <AnimatedText text={"Imagination Trumps Knowledge!"} className='mb-16' />
 
